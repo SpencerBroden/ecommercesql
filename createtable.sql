e718e448dfe1b155e238265618aba3ece32788ab
Spencer Broden <9060485+SpencerBroden@users.noreply.github.com>
2021-04-30 15:58:01 -0700

    Update createtable.sql

--- createtable.sql
+++ createtable.sql
@@ -2,8 +2,9 @@ create table "customer" (
 	customer_id serial primary key,
 	name varchar(100),
 	email varchar(320) unique not null,
-	phone varchar(15),
-	address varchar(100) not null
+	password varchar(100) not null,
+	address varchar(100) not null,
+	phone varchar(15)
 );
 
 create table "user" (
