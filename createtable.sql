2134ffe84c2ca1d14caaa3156cfb047b00869041
Spencer Broden <9060485+SpencerBroden@users.noreply.github.com>
2021-04-30 20:18:30 -0700

    Update createtable.sql

--- createtable.sql
+++ createtable.sql
@@ -15,7 +15,7 @@ create table "customer" (
 create table "login" (
 	login_id serial primary key,
 	hash varchar(100) not null,
-	customer_id integer not null references customer
+	email varchar(320) unique not null references customer
 );
 
 create table "product" (
