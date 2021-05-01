c44fdbcd6b9ddbc075cb38540ab5ad66356a9643
Spencer Broden <9060485+SpencerBroden@users.noreply.github.com>
2021-05-01 12:25:28 -0700

    Update createtable.sql

--- createtable.sql
+++ createtable.sql
@@ -1,5 +1,4 @@
 drop table "customer";
-drop table "login";
 drop table "product";
 drop table "order_product";
 
@@ -9,13 +8,8 @@ create table "customer" (
 	name varchar(100) not null,
 	email varchar(320) unique not null,
 	address varchar(100) not null,
-	phone varchar(15)
-);
-
-create table "login" (
-	login_id serial primary key,
-	hash varchar(100) not null,
-	email varchar(320) unique not null references customer(email)
+	phone varchar(15),
+	hash varchar(100)
 );
 
 create table "product" (
@@ -37,7 +31,6 @@ create table "order_product" (
 
 
 insert into customer (name, email, address) values ('Joe','joe@example.com', '111 example st');
-insert into login(hash, email) values ('password', 'joe@example.com');
 insert into product (product_name, price, units_in_stock, description) values ('Khaki Pants', 39, 100),
 ('Floral Dress', 25, 100),('Polo Shirt', 27.50, 100),('Winter Gloves', 15, 100),('Jeans', 49, 100);
 insert into order_product (quantity, total, order_date, customer_id, product_id) values (1, 26, now(), 1, 1);
