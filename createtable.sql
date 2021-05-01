c77acaa4a68a9fc33b4ca8763e4e2782d6ff0045
Spencer Broden <9060485+SpencerBroden@users.noreply.github.com>
2021-05-01 13:33:53 -0700

    Update createtable.sql

--- createtable.sql
+++ createtable.sql
@@ -1,6 +1,6 @@
 drop table "customer";
 drop table "product";
-drop table "order_product";
+drop table "orders";
 
 
 create table "customer" (
@@ -8,8 +8,8 @@ create table "customer" (
 	name varchar(100) not null,
 	email varchar(320) unique not null,
 	address varchar(100) not null,
-	phone varchar(15),
 	hash varchar(100)
+	phone varchar(15),
 );
 
 create table "product" (
@@ -20,7 +20,7 @@ create table "product" (
 	description text
 );
 
-create table "order_product" (
+create table "orders" (
 	order_id serial primary key,
 	quantity integer not null,	
 	total numeric(10,2) not null,
@@ -31,7 +31,18 @@ create table "order_product" (
 
 
 insert into customer (name, email, address) values ('Joe','joe@example.com', '111 example st');
-insert into product (product_name, price, units_in_stock, description) values ('Khaki Pants', 39, 100),
+insert into product (product_name, price, units_in_stock) values ('Khaki Pants', 39, 100),
 ('Floral Dress', 25, 100),('Polo Shirt', 27.50, 100),('Winter Gloves', 15, 100),('Jeans', 49, 100);
-insert into order_product (quantity, total, order_date, customer_id, product_id) values (1, 26, now(), 1, 1);
+insert into orders (quantity, total, order_date, customer_id, product_id) values (1, 26, now(), 1, 1);
+
+CREATE OR REPLACE FUNCTION checkCustomerEmailExists()  
+RETURNS TRIGGER AS $$
+BEGIN
+   IF NEW.email = (SELECT email FROM customer where(email = NEW.email)) THEN
+    RAISE SQLSTATE '45000';
+  END IF;
+END;
+$$ language 'plpgsql';--
+
+CREATE TRIGGER insertCustomerEmail BEFORE INSERT ON customer FOR EACH ROW EXECUTE PROCEDURE checkCustomerEmailExists();
 
